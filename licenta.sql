c140bf50991f1ae55615e2253d2c8361e4dbb365
Cosmin Tomulescu <cosmintom@yahoo.com>
2016-04-28 15:34:37 +0300

    Update the database file.

--- licenta.sql
+++ licenta.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Gazda: localhost
--- Timp de generare: 25 Apr 2016 la 10:09
+-- Timp de generare: 28 Apr 2016 la 15:32
 -- Versiune server: 5.5.49-0ubuntu0.14.04.1
 -- Versiune PHP: 5.5.9-1ubuntu4.16
 
@@ -59,20 +59,7 @@ CREATE TABLE IF NOT EXISTS `drafts` (
   `cale` varchar(200) NOT NULL,
   `uploader` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10 ;
-
---
--- Salvarea datelor din tabel `drafts`
---
-
-INSERT INTO `drafts` (`id`, `artist`, `titlu`, `cale`, `uploader`) VALUES
-(1, 'CÄƒtÄƒlina Beta', 'GÃ¢ndacul', 'drafts/cosmin_CÄƒtÄƒlina Beta_GÃ¢ndacul.txt', 'cosmin'),
-(2, 'lkj', 'lkj', 'drafts/cosmin_lkj_lkj.txt', 'cosmin'),
-(3, 'catalina beta', 'gandacul', 'drafts/cosmin_catalina beta_gandacul.txt', 'cosmin'),
-(4, 'Emilian Onciu', 'Ursoaica', 'drafts/cosmin_Emilian Onciu_Ursoaica.txt', 'cosmin'),
-(5, 'Emilian Onciu', 'Ursoaica', 'drafts/cosmin_Emilian Onciu_Ursoaica.txt', 'cosmin'),
-(6, 'Emilian Onciu', 'Ursoaica', 'drafts/cosmin_Emilian Onciu_Ursoaica.txt', 'cosmin'),
-(9, 'Bugy', 'asflakjsf', 'drafts/cosmin_Bugy_asflakjsf7.txt', 'cosmin');
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -92,7 +79,7 @@ CREATE TABLE IF NOT EXISTS `melodii` (
   `minus` int(11) NOT NULL,
   `comments_path` varchar(200) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=46 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=45 ;
 
 --
 -- Salvarea datelor din tabel `melodii`
@@ -106,10 +93,8 @@ INSERT INTO `melodii` (`id`, `artist`, `titlu`, `cale_tmp`, `cale`, `categorie`,
 (32, 'Tudor Gheorghe', 'Valsul rozelor', NULL, 'tab_uploads/cosmin_Tudor Gheorghe_Valsul rozelor.txt', 'Folclor', 'cosmin', 1, 0, 'comments/Tudor Gheorghe_Valsul rozelor_comm.txt'),
 (33, 'Bosquito', 'Bosquito', NULL, 'tab_uploads/simona_Bosquito_Bosquito.txt', 'Latino', 'simona', 1, 0, 'comments/Bosquito_Bosquito_comm.txt'),
 (34, 'Vama Veche', 'Epilog', NULL, 'tab_uploads/simona_Vama Veche_Epilog.txt', 'Dragoste', 'simona', 1, 1, 'comments/Vama Veche_Epilog_comm.txt'),
-(40, 'Catalina Beta', 'Gandacul', NULL, 'tab_uploads/cosmin_Catalina Beta_Gandacul.txt', 'Folk', 'cosmin', 0, 0, 'comments/Catalina Beta_Gandacul_comm.txt'),
-(43, 'Emilian Onciu', 'Ursoaica', NULL, 'tab_uploads/cosmin_Emilian Onciu_Ursoaica.txt', 'Folk', 'cosmin', 0, 0, 'comments/Emilian Onciu_Ursoaica_comm.txt'),
-(44, 'Emilian Onciu', 'Ursoaica', NULL, 'tab_uploads/cosmin_Emilian Onciu_Ursoaica.txt', 'Folk', 'cosmin', 0, 0, 'comments/Emilian Onciu_Ursoaica_comm.txt'),
-(45, 'Emilian Onciu', 'Ursoaica', NULL, 'tab_uploads/cosmin_Emilian Onciu_Ursoaica45.txt', 'Folk', 'cosmin', 0, 0, 'comments/Emilian Onciu_Ursoaica45_comm.txt');
+(40, 'Catalina Beta', 'Gandacul', NULL, 'tab_uploads/cosmin_Catalina Beta_Gandacul.txt', 'Folk', 'cosmin', 1, 0, 'comments/Catalina Beta_Gandacul_comm.txt'),
+(44, 'jkj', 'jlk', 'tmp_upload/jkj_jlk41.txt', NULL, 'Folclor', 'cosmin', 0, 0, 'comments/jkj_jlk4141_comm.txt');
 
 -- --------------------------------------------------------
 
@@ -125,7 +110,7 @@ CREATE TABLE IF NOT EXISTS `rating` (
   `likes` int(4) NOT NULL,
   `id` int(4) NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=59 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=60 ;
 
 --
 -- Salvarea datelor din tabel `rating`
@@ -143,7 +128,8 @@ INSERT INTO `rating` (`username`, `titlu`, `artist`, `uploader`, `likes`, `id`) 
 ('madi', 'Valsul rozelor', 'Tudor Gheorghe', 'cosmin', 1, 55),
 ('simona', 'Bosquito', 'Bosquito', 'simona', 1, 56),
 ('simona', 'Epilog', 'Vama Veche', 'simona', 1, 57),
-('madi', 'Epilog', 'Vama Veche', 'simona', 2, 58);
+('madi', 'Epilog', 'Vama Veche', 'simona', 2, 58),
+('cosmin', 'Gandacul', 'Catalina Beta', 'cosmin', 1, 59);
 
 -- --------------------------------------------------------
 
